e73f77b9bdac747eb30e40b07ac94045bb9d8287
Hidayat Halim <misterpy@gmail.com>
2013-10-27 18:10:54 +0100

    Added element for first page
    Added the clickable element on first page. No new function created yet.

--- position.js
+++ position.js
@@ -1,14 +1,14 @@
+//set the .main-places in the middle of the page
 	$(document).ready(function(){
-		$('h1').css({'width':'100%', 'text-align':'center'});
-		var h1 = $('h1').height();
-		var h = h1/2;
-		var w1 = $(window).height();
-		var w = w1/2;
+		$('.main-places').css({'text-align':'center'});
+		var h = ($('.main-places').height())/2;
+		var w = ($(window).height())/2;
 		var m = w - h;
-    	$('h1').css({'margin-top': m - 30 + 'px'});
+    	$('.main-places').css({'margin-top': m - 30 + 'px'});
     	var divw1 = $(window).width();
-    	var divw2 = $('div').width();
+    	var divw2 = $('.main-places').width();
     	var divw = divw1/2 - divw2/2 - 30;
-    	$('div').css({'margin-left' : divw + 'px'});
+    	$('.main-places').css({'margin-left' : divw + 'px'});
 	});
 
+//set the .main-<country> around the .main-places
